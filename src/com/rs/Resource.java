4788c9aa43b546fdb0d77901d41662af29f9f6dc
Miguel Krantz <miguel.krantz@telenor.se>
2018-08-18 00:43:26 +0200

    Addded support for mv4/mp4

--- src/com/rs/Resource.java
+++ src/com/rs/Resource.java
@@ -79,6 +79,8 @@ public class Resource {
     private enum ContentType {
 
         m4a("audio/mp4","m4a"), //audio/mp4
+        m4v("video/m4v","m4v"),
+        mp4("video/mp4","mp4"),
         mov("video/quicktime","mov");
 
         private final String contentType;
